fb68557e074830b874daf77e3812a847fd3a1592
Latika Sinha <sinhalatika44@gmail.com>
2022-12-25 04:53:05 +0530

    bcrypt added

--- src/utils/JWT.ts
+++ src/utils/JWT.ts
@@ -1,4 +1,5 @@
-import jwt = require("jsonwebtoken");
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const tokenSecret: string = 'thisisasecretjwttokencreatebymewhichonlyiknow';
 const expiryTime: string = '7d';
@@ -27,7 +28,35 @@ const verify = (token: string, callback: any) => {
     );
 };
 
+const encrypt = (password: string) => 
+    new Promise((resolve, reject) => {
+        bcrypt.genSalt(10, (error, salt: string) => {
+            if(error) reject({error: error, result: null})
+            bcrypt.hash(password, salt, (err, hash) => {
+                if(!err) {
+                    password = hash;
+                    resolve({error: false, result: hash});
+                } else {
+                    reject({error: err, result: null});
+                }
+            });
+        })
+    });
+
+const compare = (password: string, user_pass: string) =>
+    new Promise((resolve) => {
+        bcrypt.compare(password, user_pass, (error: any, result: any) => {
+            if(result) {
+                resolve(true);
+            } else {
+                resolve(false);
+            }
+        });
+    });
+
 export default {
     create,
     verify,
+    encrypt,
+    compare,
 }
